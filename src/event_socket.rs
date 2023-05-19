1b40d56736e5b28cbaa106873d5bd89fb24dbd98
Armin Sander <armin@replicator.org>
2023-05-19 12:19:24 +0200

    Combine reader and driver and add optional keepalives

--- src/event_socket.rs
+++ src/event_socket.rs
@@ -9,22 +9,31 @@
 //! sender push both to the same queue which gets processed by a driver.
 use crate::{
     event::{ApiResponse, AuthRequest, CommandReply, DisconnectNotice, ReplyText},
+    event_socket::driver::ProcessingResult,
     hangup_cause::HangupCause,
     query, FromMessage, Message,
 };
 use anyhow::{bail, Context, Result};
-use driver::{Command, Driver, DriverMessage, ProcessingResult};
-use event_reader::EventReader;
-use log::{debug, error};
-use std::str;
+use futures::{Future, FutureExt};
+use log::{debug, error, trace};
+use std::{future::pending, str, time::Duration};
 use tokio::{
-    net::{tcp::OwnedReadHalf, TcpStream, ToSocketAddrs},
-    spawn,
+    net::{
+        tcp::{OwnedReadHalf, OwnedWriteHalf},
+        TcpStream, ToSocketAddrs,
+    },
+    select, spawn,
     sync::{
-        mpsc::{self, Sender},
+        mpsc::{self, Receiver, Sender},
         oneshot,
     },
-    task::JoinHandle,
+    task::{JoinError, JoinHandle},
+    time,
+};
+
+use self::{
+    driver::{Command, Driver, DriverMessage},
+    event_reader::EventReader,
 };
 
 mod driver;
@@ -32,27 +41,41 @@ mod event_reader;
 
 #[derive(Debug)]
 pub struct EventSocket {
-    driver_sender: mpsc::Sender<DriverMessage>,
-    reader: JoinHandle<Result<()>>,
-    driver: JoinHandle<Result<Option<DisconnectNotice>>>,
+    driver_tx: mpsc::Sender<DriverMessage>,
+    event_socket: JoinHandle<Result<Option<DisconnectNotice>>>,
 }
 
 impl Drop for EventSocket {
     fn drop(&mut self) {
-        self.reader.abort();
-        self.driver.abort();
+        self.event_socket.abort()
+    }
+}
+
+/// `EventSocket` is itself a future that completes when the connection to the FreeSWITCH event
+/// socket is closed. It does not need to be polled for the event socket to execute.
+impl Future for EventSocket {
+    type Output = std::result::Result<Result<Option<DisconnectNotice>>, JoinError>;
+
+    fn poll(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+        self.event_socket.poll_unpin(cx)
     }
 }
 
 const MAX_QUEUE_SIZE: usize = 256;
 
 impl EventSocket {
-    pub async fn connect(endpoint: impl ToSocketAddrs) -> Result<EventSocket> {
+    pub async fn connect(
+        endpoint: impl ToSocketAddrs,
+        keepalive_interval: impl Into<Option<Duration>>,
+    ) -> Result<EventSocket> {
         let (driver_sender, driver_rx) = mpsc::channel(MAX_QUEUE_SIZE);
 
         let (tcp_receive, tcp_write) = {
             let tcp = TcpStream::connect(endpoint).await?;
-            // Send network packets out as soon as possible.
+            // Flush network packets as soon as possible.
             tcp.set_nodelay(true)?;
             tcp.into_split()
         };
@@ -64,54 +87,84 @@ impl EventSocket {
             let initial_request = reader
                 .read_event()
                 .await
-                .context("Reading the initial request")?;
+                .context("Waiting for the initial auth request")?;
             AuthRequest::from_message(initial_request).context("Expecting auth/request")?;
         }
 
-        // Spawn the event reader.
-        let reader = {
-            let driver_sender = driver_sender.clone();
-            spawn(async move {
-                let r = dispatch_events(&mut reader, driver_sender).await;
-                if let Err(ref e) = r {
-                    error!("Reader loop crashed: {}", e);
-                }
-                debug!("Ending reader loop");
-                r
-            })
-        };
-
-        // Spawn the driver.
-        let driver = spawn(async move {
-            let mut driver = Driver::new(tcp_write);
-            let mut driver_rx = driver_rx;
-            loop {
-                let message = driver_rx.recv().await;
-                if let Some(message) = message {
-                    use ProcessingResult::*;
-                    match driver.process_message(message).await? {
-                        Continue => {
-                            continue;
-                        }
-                        Disconnected(notice) => {
-                            debug!("Received disconnect notice: {}", notice);
-                            return Ok(Some(notice));
-                        }
-                    }
-                } else {
-                    debug!("Receiver channel got closed (all senders dropped)");
-                    return Ok(None);
-                }
-            }
+        // Spawn the event socket.
+        //
+        // ADR: This is for convenience so that callers don't need to drive the async function and
+        // can simply send API requests.
+        let driver_tx_socket = driver_sender.clone();
+        let keepalive_interval = keepalive_interval.into();
+        let event_socket = spawn(async move {
+            event_socket(
+                keepalive_interval,
+                reader,
+                tcp_write,
+                driver_rx,
+                driver_tx_socket,
+            )
+            .await
         });
 
         let socket = EventSocket {
-            reader,
-            driver,
-            driver_sender,
+            driver_tx: driver_sender,
+            event_socket,
         };
 
         Ok(socket)
+    }
+}
+
+/// The primary async function that represents the event socket. Its returned future must be polled
+/// until it completes indicating that the connection to the FreeSWITCH event socket terminated.
+async fn event_socket(
+    keepalives: Option<Duration>,
+    mut reader: EventReader<OwnedReadHalf>,
+    writer: OwnedWriteHalf,
+    driver_rx: Receiver<DriverMessage>,
+    driver_tx: Sender<DriverMessage>,
+) -> Result<Option<DisconnectNotice>> {
+    let reader = dispatch_events(&mut reader, driver_tx.clone());
+    let driver = dispatch_messages(driver_rx, writer);
+
+    let send_keepalives = {
+        if let Some(keepalive_duration) = keepalives {
+            debug!(
+                "Enabling keepalives with a duration of {:?}",
+                keepalive_duration
+            );
+            send_keepalives(driver_tx, keepalive_duration).boxed()
+        } else {
+            debug!("Keepalives disabled");
+            pending::<Result<()>>().boxed()
+        }
+    };
+
+    select! {
+        r = reader => {
+            debug!("Reader loop ended");
+            if let Err(ref e) = r {
+                error!("Reader loop crashed: {}", e);
+            }
+            r.map(|()| None)
+        }
+        r = driver => {
+            debug!("Driver loop ended");
+            if let Err(ref e) = r {
+                error!("Driver loop crashed: {}", e);
+            }
+            r
+        }
+        r = send_keepalives => {
+            // Note that the keepalive loop ends only in case of an error.
+            debug!("Keepalive loop ended");
+            if let Err(ref e) = r {
+                error!("Keepalive loop crashed: {}", e);
+            }
+            r.map(|()| None)
+        }
     }
 }
 
@@ -122,6 +175,41 @@ async fn dispatch_events(
     loop {
         let event = reader.read_event().await?;
         driver.send(DriverMessage::Event(event)).await?;
+    }
+}
+
+async fn dispatch_messages(
+    receiver: Receiver<DriverMessage>,
+    tcp_write: OwnedWriteHalf,
+) -> Result<Option<DisconnectNotice>> {
+    let mut driver = Driver::new(tcp_write);
+    let mut receiver = receiver;
+    loop {
+        let message = receiver.recv().await;
+        if let Some(message) = message {
+            use ProcessingResult::*;
+            match driver.process_message(message).await? {
+                Continue => {
+                    continue;
+                }
+                Disconnected(notice) => {
+                    debug!("Received disconnect notice: {}", notice);
+                    return Ok(Some(notice));
+                }
+            }
+        } else {
+            debug!("Receiver channel got closed (all senders dropped)");
+            return Ok(None);
+        }
+    }
+}
+
+async fn send_keepalives(driver_tx: Sender<DriverMessage>, duration: Duration) -> Result<()> {
+    loop {
+        time::sleep(duration).await;
+        // TODO: Really need timeouts here, what if FreeSWITCH crashes before responding?
+        trace!("Sending keepalive");
+        send_api(&driver_tx, "echo keepalive").await?;
     }
 }
 
@@ -243,11 +331,7 @@ impl EventSocket {
     /// Returns an error if the [`Message`] returned does not contain a `api/response` content
     /// block.
     pub async fn api(&self, cmd: impl AsRef<str>) -> Result<ApiResponse> {
-        let cmd = cmd.as_ref();
-        // TODO: Don't support cmd with LFs
-        // TODO: encode cmd?
-        let message = self.send(format!("api {}", cmd)).await?;
-        Ok(ApiResponse::from_message(message)?)
+        send_api(&self.driver_tx, cmd).await
     }
 }
 
@@ -260,22 +344,35 @@ impl EventSocket {
     /// This function does not implement protocol error handling. It returns the answer [`Message`]
     /// as is without looking into it.
     pub async fn send(&self, cmd: impl AsRef<str>) -> Result<Message> {
-        let (tx, rx) = oneshot::channel();
-        let command = Command::Blocking {
-            cmd: cmd.as_ref().to_owned(),
-            message: Message::default(),
-            responder: tx,
-        };
-        self.send_command(command).await?;
-        // TODO: Support timeouts here?
-        Ok(rx.await?)
+        send(&self.driver_tx, cmd).await
     }
 
     /// Sends a command.
     async fn send_command(&self, command: Command) -> Result<()> {
-        Ok(self
-            .driver_sender
-            .send(DriverMessage::Command(command))
-            .await?)
+        send_command(&self.driver_tx, command).await
     }
+}
+
+async fn send_api(driver_tx: &Sender<DriverMessage>, cmd: impl AsRef<str>) -> Result<ApiResponse> {
+    let cmd = cmd.as_ref();
+    // TODO: Don't support cmd with LFs
+    // TODO: encode cmd?
+    let message = send(driver_tx, format!("api {}", cmd)).await?;
+    Ok(ApiResponse::from_message(message)?)
+}
+
+async fn send(driver_tx: &Sender<DriverMessage>, cmd: impl AsRef<str>) -> Result<Message> {
+    let (tx, rx) = oneshot::channel();
+    let command = Command::Blocking {
+        cmd: cmd.as_ref().to_owned(),
+        message: Message::default(),
+        responder: tx,
+    };
+    send_command(driver_tx, command).await?;
+    // TODO: Support timeouts here?
+    Ok(rx.await?)
+}
+
+async fn send_command(driver_tx: &Sender<DriverMessage>, command: Command) -> Result<()> {
+    Ok(driver_tx.send(DriverMessage::Command(command)).await?)
 }
